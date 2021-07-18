9ed3a08990ef2c283e432259b303754f0e7fe944
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2021-07-18 20:29:56 +0100

    more chnages to models (still broken)

--- core/lua-scripts/models.lua
+++ core/lua-scripts/models.lua
@@ -4,10 +4,15 @@
 
 local r = reaper
 
+local function msg(object)
+    r.ShowConsoleMsg(tostring(object) .. '\n')
+end
+
+
 -- FX
 FX = {
-    idx = nil,
-    media_track = nil
+    media_track = nil,
+    idx = nil
 }
 
 function FX:new(media_track, idx)
@@ -21,10 +26,11 @@ end
 
 function FX:__tostring()
     return string.format(
-        'FX <name=%s, is_instrument=%s, is_enabled=%s, is_offline=%s>',
+        'FX <idx=%s, name=%s, is_instrument=%s, is_enabled=%s, is_offline=%s>',
+        self.idx,
         self:get_name(),
-        self:is_enabled(),
         self:is_instrument(),
+        self:is_enabled(),
         self:is_offline()
     )
 end
@@ -38,7 +44,12 @@ end
 function FX:get_name()
     -- Get FX Name
     -- @return string
-    return r.TrackFX_GetFXName(self.media_track, self.idx, '')
+    local retval, name = r.TrackFX_GetFXName(self.media_track, self.idx, '')
+    if retval then
+        return name
+    else
+        return nil
+    end
 end
 
 function FX:is_enabled()
@@ -56,12 +67,16 @@ end
 function FX:is_instrument()
     -- Whether FX is a virtual instrument
     -- @return boolean
-    if r.TrackFX_GetInstrument(self.media_track) == self.idx then
+    local inst_idx = r.TrackFX_GetInstrument(self.media_track)
+    if inst_idx == -1 then
+        return false
+    elseif inst_idx == self.idx then
         return true
     end
     local patterns = {'VSTi', 'VST3i', 'AUi'}
+    local name = self:get_name()
     for _, p in pairs(patterns) do
-        if self.get_name():find(p) then
+        if name:find(p) then
             return true
         end
     end
@@ -70,20 +85,21 @@ end
 
 function FX:enable()
     -- Enable FX
-    r.TrackFX_SetEnabled(self.media_track, self.idx, true)
+    r.TrackFX_SetEnabled(self.idx, true)
 end
 
 function FX:disable()
     -- Disable FX
-    r.TrackFX_SetEnabled(self.media_track, self.idx, false)
+    r.TrackFX_SetEnabled(self.idx, false)
 end
 
 -- Track
 Track = {
-    media_track = nil
+    media_track = nil,
+    fx_chain = {}
 }
 
-function Track:new(media_track --[[MediaTrack]])
+function Track:new(media_track)
     local o = {}
     setmetatable(o, self)
     self.__index = self
@@ -112,18 +128,29 @@ function Track:fx_count()
     return r.TrackFX_GetCount(self.media_track)
 end
 
-function Track:get_fx_chain()
+function Track:set_fx_chain()
     -- get all FX for a given track
     -- @return table
+    msg('in get_fx_chain')
     local fx_chain = {}
-    for fx_idx = 0, self.fx_count() - 1 do
-        local fx = FX:new({
-            idx = fx_idx,
-            media_track = self.media_track
-        })
-        fx_chain[fx_idx] = fx
+    local fx_count = self:fx_count()
+    msg('self fx_count' .. fx_count)
+    for fx_idx = 0, fx_count - 1 do
+        self.fx_chain[fx_idx + 1] = FX:new(self.media_track, fx_idx)
     end
-    return fx_chain
+end
+
+function Track:get_fx_chain()
+    return self.fx_chain
+end
+
+function Track:has_instrument()
+    for _, fx in ipairs(self.get_fx_chain()) do
+        if fx:is_instrument() then
+            return true
+        end
+    end
+    return false
 end
 
 function Track:disable_all_fx(include_inst --[[boolean]])
@@ -132,7 +159,7 @@ function Track:disable_all_fx(include_inst --[[boolean]])
     for _, fx in pairs(self.get_fx_chain()) do
         if include_inst then
             fx:disable()
-        elseif not fx.is_instrument() then
+        elseif not fx:is_instrument() then
             fx:disable()
         end
     end
--- core/lua-scripts/render_instrument.lua
+++ core/lua-scripts/render_instrument.lua
@@ -2,36 +2,56 @@
 -- @author NomadMonad
 -- @version 0.1a
 --
-function reaperDoFile(file) 
+local r = reaper
+
+local function msg(object)
+    r.ShowConsoleMsg(tostring(object) .. '\n')
+end
+
+local function reaper_dofile(file) 
     local info = debug.getinfo(1,'S')
     local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
     dofile(script_path .. file)
 end
 
-reaperDoFile("models.lua")
+reaper_dofile("models.lua")
 
-local function msg(str)
-    r.ShowConsoleMsg(str .. '\n')
+
+local function reload_fx_chain_state(track, fx_chain_state)
+    for i, fx in track.get_fx_chain() do
+        local old_state = fx_chain_state[i]
+        msg(fx)
+        msg(old_state)
+    end
 end
 
-local r = reaper
+local function main()
+    r.Undo_BeginBlock2(1)
+    r.PreventUIRefresh(1)
+    if r.CountSelectedMediaItems(0) == 0 then
+        r.MB('Please select an item', 'Error', 0)
+        return
+    end
+    local sel_media_track = r.GetSelectedTrack(0, 0)
+    local sel_track = Track:new(sel_media_track)
+    local fx_chain = sel_track:get_fx_chain()
+    msg('-----')
+    for _, fx in ipairs(fx_chain) do
+        msg('in script')
+        msg('idx ' .. _ .. ' fx ' .. tostring(fx))
 
-r.Undo_BeginBlock(1)
-r.PreventUIRefresh(1)
+    end
 
-if r.CountSelectedMediaItems(0) == 0 then
-    r.MB('Please select an item', 'Error', 0)
-    return
+    -- if not sel_track:has_instrument() then
+    --     r.MB('Track has no virtual instrument', 'Error', 0)
+    --     return
+    -- end
+    -- local fx_chain_state = sel_track:get_fx_chain()
+    -- sel_track:disable_all_fx()
+    -- reload_fx_chain_state(sel_track, fx_chain_state)
+    -- r.Main_OnCommand(40209, 0)
+    -- r.PreventUIRefresh(-1)
+    -- r.Undo_EndBlock2('Render instrument to stereo take', -1)
 end
-local sel_media_track = r.GetSelectedTrack(0, 0)
-local sel_track = Track:new(sel_media_track)
-if not sel_track.has_instrument then
-    r.MB('Track has no virtual instrument', 'Error', 0)
-    return
-end
-sel_track:disable_all_fx()
-r.Main_OnCommand(40209, 0)
-sel_track:state_reload()
 
-r.PreventUIRefresh(-1)
-r.Undo_EndBlock('Apply to stereo take', -1)
+main()
