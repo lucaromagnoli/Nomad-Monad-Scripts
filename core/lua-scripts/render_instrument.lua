651160e606aa587723d4f504d6be9086f52604e1
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2021-07-23 23:07:58 +0100

    major refactoring

--- core/lua-scripts/render_instrument.lua
+++ core/lua-scripts/render_instrument.lua
@@ -2,38 +2,14 @@
 -- @author NomadMonad
 -- @version 0.1a
 --
-local r = reaper
-
-local function msg(object)
-    r.ShowConsoleMsg(tostring(object) .. '\n')
-end
-
 DEBUG = true
 
--- local function log(...)
---     if DEBUG then
---         local joined = ''
---         for _, v in ipairs(arg) do
---             if joined then
---                 joined = joined .. ', ' .. tostring(v)
---             else
---                 joined = tostring(v)
---             end
---             msg(joined)
---         end
---     end
--- end
+local path = ({reaper.get_action_context()})[2]:match('^.+[\\//]')
+package.path = path .. "?.lua"
 
+require('models')
 
-local function reaper_dofile(file) 
-    local info = debug.getinfo(1,'S')
-    local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
-    dofile(script_path .. file)
-end
-
-reaper_dofile("models.lua")
-
-
+local r = Reaper:new()
 
 local function bypass_fx_chain(fx_chain)
     for _, fx in ipairs(fx_chain) do
@@ -54,31 +30,26 @@ local function reload_fx_chain_state(fx_chain)
     end
 end
 
-
+local function bypass_render_reload(track)
+    local fx_chain = track:get_fx_chain()
+    bypass_fx_chain(fx_chain)
+    r:apply_fx()
+    reload_fx_chain_state(fx_chain)
+end
 
 local function main()
-    if r.CountSelectedMediaItems(0) == 0 then
-        r.MB('Please select an item', 'Error', 0)
+    local p = Project:new()
+    if p:count_selected_media_items() == 0 then
+        r:msg_box('Please select an item', 'Error', 0)
         return
     end
-    local sel_media_track = r.GetSelectedTrack(0, 0)
-    local sel_track = Track:new(sel_media_track)
-    local fx_chain = sel_track:get_fx_chain()
-    msg(sel_track)
-    msg(fx_chain)
-    local has_instrument = sel_track:has_instrument()
-    if has_instrument then
-        r.Undo_BeginBlock(1)
-        r.PreventUIRefresh(1)
-        bypass_fx_chain(fx_chain)
-        r.Main_OnCommand(40209, 0) -- render to stereo take
-        reload_fx_chain_state(fx_chain)
-        r.PreventUIRefresh(-1)
-        r.Undo_EndBlock('Render to stereo take', -1)
-    else
-        r.MB('Track has no instrument', 'Error', 0)
+    for _, sel_track in ipairs(p:get_selected_tracks()) do
+        if sel_track:has_instrument() then
+            r:undo('Render instrument', bypass_render_reload, sel_track)
+        else
+            r:msg_box('Track has no instrument', 'Error', 0)
+        end
     end
-
 end
 
 main()
