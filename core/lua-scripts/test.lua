9f2dbffa7ae0ebdb259f3ebd610acf39062d6f65
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2021-07-25 22:22:09 +0100

    remove unused things

--- core/lua-scripts/test.lua
+++ core/lua-scripts/test.lua
@@ -2,10 +2,3 @@ local path = ({reaper.get_action_context()})[2]:match('^.+[\\//]')
 package.path = package.path .. ';' .. path .. '?.lua'
 package.path = package.path .. ';' .. path .. 'ReaWrap/models/?.lua'
 require('ReaWrap.models')
-
-local r = Reaper:new()
-local p = Project:new()
-
-for _, track in ipairs(p:get_selected_tracks()) do
-    r:print(track)
-end
