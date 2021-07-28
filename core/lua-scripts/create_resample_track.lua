3d05654373c115e9a8bd1dd1b05402d248d3d548
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2021-07-28 10:58:35 +0100

    working version

--- core/lua-scripts/create_resample_track.lua
+++ core/lua-scripts/create_resample_track.lua
@@ -12,9 +12,44 @@ LSep = '['
 RSep = ']'
 ResampleTrackPrefix = LSep .. DefaultTag .. RSep
 ResampleTrackPrefixEscaped = '%'.. LSep .. DefaultTag .. '%' .. RSep
+SourceTrackState = 'SourceTrackState'
 
 local r = Reaper:new()
 local p = Project:new()
+
+
+-- Get the resample_track mapped to source_track from project key-value store.
+local function get_rsmpl_track(project, source_track)
+    local rsmpl_guid = project:get_key_value(DefaultTag, source_track:GUID())
+    return p:track_from_guid(rsmpl_guid)
+end
+
+-- Create a mapping from source track to resample_track in project key-value store.
+local function set_rsmpl_track(project, source_track, resample_track)
+     project:set_key_value(
+             DefaultTag, source_track:GUID(), resample_track:GUID(), true
+     )
+end
+
+-- Whether source_track has a RSMPL track mapped to it in project key-value store.
+local function has_rsmpl_track(project, source_track)
+    return project:has_key_value(DefaultTag, source_track:GUID())
+end
+
+-- Delete mapping from source track to resample_track in project key-value store.
+local function del_rsmpl_track(project, source_track)
+    project:del_key_value(DefaultTag, source_track:GUID(), true)
+end
+
+-- Save source_track state in project key-value store.
+local function set_source_track_state(project, source_track)
+    project:set_key_value(
+            SourceTrackState,
+            source_track:GUID(),
+            source_track:get_state_chunk().raw,
+            true
+    )
+end
 
 ResampleTrack = Track:new()
 
@@ -38,17 +73,21 @@ function ResampleTrack:__tostring()
     )
 end
 
-
 function ResampleTrack:from_source_track(project, source_track)
+    set_source_track_state(project, source_track)
     local color = source_track:get_color()
-    local index = source_track:get_index()
+    local index = source_track:get_index(0)
     local name = ResampleTrackPrefix .. source_track:get_name()
-    local track = project:add_track(index, false)
+    local track = project:add_track(index + 1, false)
     track:set_name(name)
     track:set_color(color)
-    local rsmpl = self:new(track.media_track, source_track)
-    project:set_key_value(DefaultTag, source_track:GUID(), rsmpl:GUID(), true)
-    return rsmpl
+    local rsmpl_track = self:new(track.media_track, source_track)
+    local send = source_track:create_send(rsmpl_track)
+    send:set_info_value(SendReceiveInfoValue.I_MIDIFLAGS, 0)
+    send:set_info_value(SendReceiveInfoValue.I_SENDMODE, 3) -- pre fader
+    source_track:set_info_value(TrackInfoValue.B_MAINSEND, 0)
+    set_rsmpl_track(project, source_track, rsmpl_track)
+    return rsmpl_track
 end
 
 local function get_inst_and_fx(track)
@@ -64,32 +103,15 @@ local function get_inst_and_fx(track)
     return instruments, audio_fx
 end
 
-local function move_fx_chain(source_track, rsmpl_track, index)
-    index = index or 1
+local function move_fx_chain(source_track, rsmpl_track, fx_index)
+    fx_index = fx_index or 1
     local fx_count = source_track:get_fx_count()
-    while  fx_count > index do
+    while  fx_count > fx_index do
         local fx = TrackFX:new(source_track, fx_count - 1)
-        fx_count = fx_count - 1
         fx:move_to_track(rsmpl_track, 0)
+        fx_count = fx_count - 1
     end
 end
-
-
-local function get_rsmpl_track(project, track)
-    local rsmpl_guid = project:get_key_value(DefaultTag, track:GUID())
-    return project:track_from_guid(rsmpl_guid)
-end
-
-
-local function has_rsmpl_track(project, track)
-    return project:has_key_value(DefaultTag, track:GUID())
-end
-
-
-local function del_rsmpl_track(project, track)
-    project:del_key_value(DefaultTag, track:GUID(), true)
-end
-
 
 local function unlink_from_rsmpl_track(project, track, rsmpl_track)
     local rsmpl_name = rsmpl_track:get_name()
@@ -132,26 +154,25 @@ local function check_rsmpl_track(project, source_track)
 end
 
 -- Create a table of RSMPLTrack from currently selected tracks.
---@return Table<RSMPLTrack>
-local function create_rsmpl_tracks(project, index)
+-- @return Table<RSMPLTrack>
+local function create_rsmpl_tracks(project, fx_index)
     local resample_tracks = {}
     for source_track in project:iter_selected_tracks() do
         if check_rsmpl_track(project, source_track) then
             local resample_track = ResampleTrack:from_source_track(project, source_track)
-            move_fx_chain(source_track, resample_track, index)
+            move_fx_chain(source_track, resample_track, fx_index)
             resample_tracks[#resample_tracks + 1] = resample_track
         end
     end
     return resample_tracks
 end
 
-
 local function main(opts)
     if not p:has_selected_tracks()
         then r:msg_box('Please select a track', 'No track selected')
         return
     end
-    for _, rsmpl in ipairs(create_rsmpl_tracks(p)) do
+    for _, rsmpl in ipairs(create_rsmpl_tracks(p, fx_index)) do
         r:print(rsmpl)
     end
 end
