66a113b0d9113f0ca71b6ef8606535bca612e051
Luca Romagnoli <luca.romagnoli@sainsburys.co.uk>
2021-08-13 23:49:18 +0200

    add TestIterPluginsAll

--- core/lua-scripts/utils/test_plugin.lua
+++ core/lua-scripts/utils/test_plugin.lua
@@ -6,11 +6,21 @@ local plugins_manager = PluginsManager:init('ini_data', 'OSX')
 function TestPluginsMap()
     for _, v in ipairs({'VST', 'VST3', 'VSTi', 'VST3i', 'AU', 'AUi', 'JS'}) do
         assert(plugins_manager.plugins_map[v] ~=nil)
-        print(v)
-        print(#plugins_manager.plugins_map[v])
         assert(#plugins_manager.plugins_map[v] > 0)
     end
 end
+
+
+function TestIterPluginsAll()
+    local unique = {}
+    for i, plugin in plugins_manager:iter_plugins() do
+        unique[plugin.format] = true
+    end
+    for i, s in ipairs(Plugin.formats) do
+        assert(unique[s] ~= nil)
+    end
+end
+
 
 
 local runner = lu.LuaUnit.new()
